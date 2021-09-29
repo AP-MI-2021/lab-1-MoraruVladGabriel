bc0244241bf59ac7219778ba79e94bd17dff8db9
moraru vlad <moraruvlad51@yahoo.com>
2021-09-29 19:54:00 +0300

    primul comit

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
   # codul vostru aici
+  '''
+  Determina daca un numar este prim.
+  :param n: intreg
+  :return: True , daca n este prim ; False daca n nu este prim.
+  '''
+
+  if n < 2 :
+    return False
+  else :
+    for i in range(2,n//2 +1) :
+      if n % i == 0 :
+        return False
+    return True
   
   
 '''
@@ -10,7 +23,16 @@ Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
-  
+  '''
+  Determina produsul elementelor dintr-o lista.
+  :param lst: tablou unidimensional
+  :return: produsul elementelor din lista
+  '''
+  prod = 1
+  for i in range(len(lst)):
+    prod = prod * lst[i]
+
+  return prod
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
